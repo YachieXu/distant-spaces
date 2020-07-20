c0dc12647a532b3753fe901313d3a18ae122cba0
ntnox <quicksilverdust@gmail.com>
2020-07-20 15:26:28 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.use(bodyParser.json());
 app.use(express.static('images'));
 const config = require("./config.json");
 
+
+//set both access tokens
+var bottoken = config.token;
+var dnatoken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYnkiOiJMb2NhdGlvbiIsInR5cGUiOiJCZWFyZXIiLCJ0ZW5hbnRJZCI6MTc3NjksInVzZXJuYW1lIjoieWFjeHVAY2lzY28uY29tIiwia2V5SWQiOiI3M2FkYWQ0NC0yYjQwLTRhOTEtOGMwOS0zYzUxZjQ5YmQwMDYiLCJ1c2VySWQiOjI0MjY3LCJpYXQiOjE1OTQwODkzMzMsImV4cCI6MTYwMTg2NTMzM30.1gS2jWgara12uYQwsqhwDIYYN9lhGNnSXv01JCrrxhs"
+
+//set email of office admin
+var officeadminemail = "kelvinwhcui@gmail.com"
+
+
+var cardstatus = 0; //guide - 0 is default, and triggers the floor_output_card when action.submit is called, 1 triggers office_admin_card when action.submit is called, 2 triggers a dm to the office admin
+
+
 // init framework
 var framework = new framework(config);
 framework.start();
@@ -31,7 +43,7 @@ framework.on('spawn', (bot, id, actorId) => {
     // Lets find out more about them..
     var msg = 'You can say `help` to get the list of words I am able to respond to.';
     bot.webex.people.get(actorId).then((user) => {
-      msg = `Hello there ${user.displayName}. ${msg}`; 
+      msg = `Hello there! ${msg}`; 
     }).catch((e) => {
       console.error(`Failed to lookup user details in framwork.on("spawn"): ${e.message}`);
       msg = `Hello there. ${msg}`;  
@@ -72,57 +84,106 @@ framework.hears('Client Count', function (bot) {
   console.log("Client Count in the location");
   responded = true;
   //There would be an additional API call here to verify how many floors exist in the DNASpaces account, and set options for the floor_input_card
-  bot.sendCard(floor_input_card, 'This is customizable fallback text for clients that do not support buttons & cards'); //Send card in message, with card_dice_JSON defined below - second string is displayed when card is not supported
+  bot.sendCard(floor_input_card, 'Floor Count Selector Card'); //Send card in message, with card_dice_JSON defined below - second string is displayed when card is not supported
+  cardstatus = 0;
+});
 
-  /* ----- Old Code -------------
-  console.log("Client Count in the location");
+framework.hears('Office Admin', function (bot) {
+  console.log("Office Admin");
   responded = true;
-
-  var myHeaders = newHeaders ();
-  myHeaders.append("Content.Type", "application/json");
-  myHeaders.append("Authorization", "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYnkiOiJMb2NhdGlvbiIsInR5cGUiOiJCZWFyZXIiLCJ0ZW5hbnRJZCI6MTc3NjksInVzZXJuYW1lIjoieWFjeHVAY2lzY28uY29tIiwia2V5SWQiOiI3M2FkYWQ0NC0yYjQwLTRhOTEtOGMwOS0zYzUxZjQ5YmQwMDYiLCJ1c2VySWQiOjI0MjY3LCJpYXQiOjE1OTQwODkzMzMsImV4cCI6MTYwMTg2NTMzM30.1gS2jWgara12uYQwsqhwDIYYN9lhGNnSXv01JCrrxhs")
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-
-  var output = fetch("https://dnaspaces.io/api/location/v1/clients/count", requestOptions)
-    .then (response=> response.json())
-    .then (data=>{
-      var total = data.results.total
-      console.log ('Client count is ${total}')
-      bot.say('The number of clients in the current location is: ${total}');
-    })
-    .then (result => console.log(result))
-    .catch (error => console.log('error', error));
-
-    ------------------old code end--------------------------------*/
+  //There would be an additional API call here to verify how many floors exist in the DNASpaces account, and set options for the floor_input_card
+  bot.sendCard(office_admin_card, 'Office Admin Information'); //Send card in message, with card_dice_JSON defined below - second string is displayed when card is not supported
+  cardstatus = 2;
 });
 
 framework.on('attachmentAction', function (bot, trigger) {
-  //initalizing variables 
-  let floor = "";
-  let count = 0;
-  let status = "";
+  let personname = trigger.person.displayName;
+  let personemail = trigger.person.emails[0];
 
-  let placeholdercount = {'26':7, '27':18, '28':25}; //this would be replaced with a api call
 
-  let input = JSON.parse(JSON.stringify(trigger.attachmentAction, null, 2)); //parse data from previous card
-  floor = input.inputs.floor //find which floor user selected
+  if (cardstatus == 0) {//send floor_output_card
+    //variable definitions
+    let floor = "";
+    let count = 0;
+    let status = "";
+    let floorcount = {};
 
-  let messageid = input.messageId; //find message id from previous card
-  deletemsg(messageid); //delete previous card
 
-  count = placeholdercount[floor]; //find the cound assosiated with the floor chosen
-  status = getstatus(count); //set "status" in output card
-  console.log("Status on floor " + floor + " is " + status);
-  (floor_output_card).body[0].columns[0].items[1].text = "Floor " + floor; //set floor number in output card
-  (floor_output_card).body[2].items[0].text = "Number of people on this floor: " + count; //set number of people in output card
-  bot.sendCard((floor_output_card), 'Placeholder'); //send to end user
+    //floorkey - use this to convert floor id to human readable floor numbers
+    let floorkey = {"719d02e2b00e4535b6e095c0d757e44b" : "26", } 
+
+    //date - use this object to get the time of the query
+    let date = new Date();
+    let time = date.toUTCString();
+
+    //start api call to dnaspaces to query all counts on all floors
+    const https = require('https');
+    const options = {
+      hostname: 'dnaspaces.io',
+      path: '/api/location/v1/clients/floors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'bearer ' + dnatoken
+      },
+    }
+    https.get(options, (response) => {
+    console.log(`statusCode: ${response.statusCode}`)
+
+    let data = ''
+    response.on('data', function (chunk) {
+        data += chunk;
+    });
+
+    response.on('end', function () { 
+        let client_res = (JSON.parse(data))
+        //iterating over all floors, adding the count of each floor to the floorcount object
+        for (let i of client_res.results) {
+          let floorid = i.floorID;
+          let floor = floorkey[floorid]; //convert floorid into human readable floor numbers using floorkey object
+          floorcount[floor] = parseInt(i.count); 
+        }
+    });
+
+    response.on("error", function (error) {
+      console.error(error);
+      });
+    });
+
+
+    floorcount = {"26":8, "27":17, "28":25}; //DEMO ONLY - remove this line if using api
+
+    let input = JSON.parse(JSON.stringify(trigger.attachmentAction, null, 2)); //parse data from previous card
+    floor = input.inputs.floor //find which floor user selected
+    console.log("floor is " + floor)
+
+    let messageid = input.messageId; //find message id from previous card
+    deletemsg(messageid); //delete previous card
+
+    count = floorcount[floor]; //find the cound assosiated with the floor chosen
+    status = getstatus(count); //set "status" in output card
+    console.log("Status on floor " + floor + " is " + status);
+    floor_output_card.body[0].columns[0].items[1].text = "Floor " + floor; //set floor number in output card
+    floor_output_card.body[2].items[0].text = "Number of people on this floor: " + count; //set number of people in output card
+    floor_output_card.body[5].items[0].text = "Time: " + time;
+    bot.sendCard((floor_output_card), 'Floor Count Card'); //send to end user
+    cardstatus = 1;
+  }
+  else if (cardstatus == 1){//send out office_admin_card
+    bot.sendCard((office_admin_card), 'Office Admin Card')
+    cardstatus = 2;
+  }
+
+  else if (cardstatus == 2){
+    let roomid = ""
+    console.log("Creating room!");
+    createroom(personname, personemail)
+  }
+
 });
 
+// ------------------Helper Functions --------------------------------------
 
-// ------------------Helper Functions for Number of Clients --------------------------------------
+//--------------------Get status based on people count function---------------
 function getstatus(count) {
   if (count < 10) { //when there are less than 10 people
     (floor_output_card).body[1].items[0].text = "Status : Safe";
@@ -147,13 +208,14 @@ function getstatus(count) {
 
 }
 
+//---------------Delete a message function------------------------
 function deletemsg(messageid){
   const https = require('https') //start https
   const options = {
     hostname: 'webexapis.com', 
     path: '/v1/messages/' + messageid, //messageid from message you'd want to delete
     method: 'DELETE', //this API calls for DELETE - can be subsituted for GET or POST or PUT depending on what API requires
-    headers: {"Authorization" : "Bearer "  + token} //set authorization in header - "Bearer" lets the API know that it's OAuth and not HTTP Basic Auth
+    headers: {"Authorization" : "Bearer "  + bottoken} //set authorization in header - "Bearer" lets the API know that it's OAuth and not HTTP Basic Auth
   }
   
   const req = https.request(options, (res) => {
@@ -169,10 +231,104 @@ function deletemsg(messageid){
   })
   
   req.end()
+}
+//---------------------Create a Room function ------------------
+function createroom(personname, personemail){
+  console.log("creating room!")
+  const https = require('https');
+  let roomid = ""
+  
+  var postData = JSON.stringify(
+    {
+    "title": personname + "'s Office Admin Request"
+    }
+  );
+  
+  var options = {
+    hostname: 'webexapis.com',
+    path: '/v1/rooms',
+    method: 'POST',
+    headers: {
+         'Content-Type': 'application/json',
+         'Content-Length': postData.length,
+         "Authorization" : "Bearer "  + bottoken
+       }
+  };
+ 
+  var req = https.request(options, (res) => {
+    //console.log('statusCode:', res.statusCode);
+    //console.log('headers:', res.headers);
 
+    let data = '';
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      data = JSON.parse(data);
+      //console.log("data" + data);
+      roomid = data.id;
+      //console.log("data.id: " + data.id);
+      addroom(roomid, officeadminemail);
+      addroom(roomid, personemail);
+    });
+  });
+
+  req.on('error', (e) => {
+    console.error(e);
+  });
+  
+  req.write(postData);
+  req.end();
 }
 
-//---------------------------------Card JSON for number of clients----------------------------
+//--------------------Add people to a room subfunction-------------
+function addroom(roomid, personemail) {
+  console.log("Adding " + personemail + " to room with id " + roomid)
+  const https = require('https');
+  
+  var postData = JSON.stringify(
+    {
+    "roomId": roomid,
+    "personEmail": personemail
+    }
+  );
+  
+  var options = {
+    hostname: 'webexapis.com',
+    path: '/v1/memberships',
+    method: 'POST',
+    headers: {
+         'Content-Type': 'application/json',
+         'Content-Length': postData.length,
+         "Authorization" : "Bearer "  + bottoken
+       }
+  };
+ 
+  var req = https.request(options, (res) => {
+    //console.log('statusCode:', res.statusCode);
+    //console.log('headers:', res.headers);
+
+    let data = '';
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      data = JSON.stringify(data);
+      //console.log(data);
+    });
+  });
+
+  req.on('error', (e) => {
+    console.error(e);
+  });
+  
+  req.write(postData);
+  req.end();
+}
+
+//---------------------------------Card JSONs----------------------------
 let floor_input_card = 
 {
   "type": "AdaptiveCard",
@@ -298,7 +454,7 @@ let floor_output_card =
           "items": [
               {
                   "type": "TextBlock",
-                  "text": "Status : ",
+                  "text": "Status: ",
                   "id": "status"
               }
           ]
@@ -308,8 +464,88 @@ let floor_output_card =
           "items": [
               {
                   "type": "TextBlock",
-                  "text": "Number of people on this floor : ",
+                  "text": "Number of people on this floor: ",
                   "id": "number"
+              }
+          ],
+          "separator": true
+      },
+      {
+          "type": "Container",
+          "items": [
+              {
+                  "type": "TextBlock",
+                  "text": "Sanitization Stations are located in the following areas:",
+                  "id": "sanitization",
+                  "wrap": true,
+                  "weight": "Lighter"
+              },
+              {
+                  "type": "TextBlock",
+                  "text": "Lobby, Cafeteria, Mail Room"
+              },
+              {
+                  "type": "Container",
+                  "items": [
+                      {
+                          "type": "TextBlock",
+                          "text": "Please visit WHO for the latest advice regarding COVID-19:",
+                          "weight": "Lighter",
+                          "id": "who",
+                          "wrap": true,
+                          "maxLines": 1
+                      },
+                      {
+                          "type": "ActionSet",
+                          "actions": [
+                              {
+                                  "type": "Action.OpenUrl",
+                                  "title": "World Health Organization",
+                                  "iconUrl": "https://img.icons8.com/windows/32/000000/world-health-organization.png",
+                                  "url": "https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public"
+                              }
+                          ],
+                          "spacing": "None",
+                          "horizontalAlignment": "Left"
+                      }
+                  ],
+                  "separator": true
+              }
+          ],
+          "separator": true
+      },
+      {
+          "type": "Container",
+          "items": [
+              {
+                  "type": "TextBlock",
+                  "text": "WRP Resources:",
+                  "weight": "Lighter",
+                  "spacing": "None"
+              },
+              {
+                  "type": "ActionSet",
+                  "horizontalAlignment": "Left",
+                  "actions": [
+                      {
+                          "type": "Action.Submit",
+                          "title": "Office Admin Info",
+                          "iconUrl": "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-contact-512.png"
+                      }
+                  ],
+                  "spacing": "None"
+              }
+          ]
+      },
+      {
+          "type": "Container",
+          "items": [
+              {
+                  "type": "TextBlock",
+                  "text": "Time:",
+                  "size": "Small",
+                  "weight": "Lighter",
+                  "color": "Accent"
               }
           ],
           "separator": true
@@ -319,43 +555,73 @@ let floor_output_card =
   "version": "1.2"
 }
 
-// maintnance areas
-
-framework.hears('Maintnance Areas', function (bot) {
-  console.log("Avoided Areas");
-  responded = true;
-  var myHeaders = newHeaders ();
-  myHeaders.append("Content.Type", "application/json");
-  myHeaders.append("Authorization", "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYnkiOiJMb2NhdGlvbiIsInR5cGUiOiJCZWFyZXIiLCJ0ZW5hbnRJZCI6MTc3NjksInVzZXJuYW1lIjoieWFjeHVAY2lzY28uY29tIiwia2V5SWQiOiI3M2FkYWQ0NC0yYjQwLTRhOTEtOGMwOS0zYzUxZjQ5YmQwMDYiLCJ1c2VySWQiOjI0MjY3LCJpYXQiOjE1OTQwODkzMzMsImV4cCI6MTYwMTg2NTMzM30.1gS2jWgara12uYQwsqhwDIYYN9lhGNnSXv01JCrrxhs")
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-
-  var output = fetch("https://dnaspaces.io/api/location/v1/map/elements/floor", requestOptions)
-    .then (response=> response.json())
-    .then (data=>{
-      var total = data.results.total
-      console.log ('Avoid the following floors ${total}')
-      bot.say(' Please avoid the following areas: ${total}');
-    })
-    .then (result => console.log(result))
-    .catch (error => console.log('error', error));
-});
-
-//Office Admin
-framework.hears('Office Admin', function (bot) {
-	  console.log('Office Admin Info Requested');
-	  responded = true;
-	  bot.say('The Office Admin is Tara Ward');
-	});
-
-//Sanitization Stations
-framework.hears('Sanitization Stations', function (bot) {
-	  console.log('Sanitization Station info requested');
-	  responded = true;
-	  bot.say('Sanitization Stations are located at the following: lobby Area, cafeteria, mail room');
-	});
+let office_admin_card = 
+{
+  "type": "AdaptiveCard",
+  "version": "1.0",
+  "body": [
+      {
+          "type": "Container",
+          "items": [
+              {
+                  "type": "TextBlock",
+                  "text": "Distant Spaces Demo",
+                  "color": "Accent",
+                  "weight": "Lighter"
+              }
+          ]
+      },
+      {
+          "type": "Container",
+          "items": [
+              {
+                  "type": "TextBlock",
+                  "text": "Office Admin",
+                  "size": "Large",
+                  "weight": "Bolder"
+              }
+          ]
+      },
+      {
+          "type": "Container",
+          "items": [
+              {
+                  "type": "TextBlock",
+                  "text": "The office admin for this space is:"
+              },
+              {
+                  "type": "TextBlock",
+                  "text": "Tara Ward",
+                  "weight": "Bolder"
+              }
+          ]
+      },
+      {
+          "type": "Container",
+          "separator": true,
+          "items": [
+              {
+                  "type": "TextBlock",
+                  "text": "Contact Information:"
+              },
+              {
+                  "type": "TextBlock",
+                  "text": "Email: taward@cisco.com"
+              },
+              {
+                  "type": "ActionSet",
+                  "actions": [
+                      {
+                          "type": "Action.Submit",
+                          "title": "Start a Conversation"
+                      }
+                  ]
+              }
+          ]
+      }
+  ],
+  "$schema": "http://adaptivecards.io/schemas/adaptive-card.json"
+}
 
 
 /* On mention with unexpected bot command
@@ -373,13 +639,9 @@ framework.hears(/.*/, function (bot, trigger) {
 });
 
 function sendHelp(bot) {
-  bot.say("markdown", 'These are the commands I can respond to:', '\n\n ' +
-    '1. **Client Count**   (clients per floor) \n' +
-    '2. **Maintnance Areas**  (Maintnace zones in process of being cleaned) \n' +
-    '3. **Office Admin**  (get details about office admin) \n' +
-    '4. **Sanitization Stations** (get details about the location of sanitization stations) \n' );
-
-}
+  bot.say("markdown", 'If you are looking for floor density updates, please enter:', '\n\n ' +
+    '**Client Count**\n' 
+    )}
 
 
 //Server config & housekeeping
